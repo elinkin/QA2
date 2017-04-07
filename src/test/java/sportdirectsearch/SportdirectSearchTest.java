ec03f8a8575bd4869ba8a32e47d842d54f594f16
elinkin <elinkin@gmail.com>
2017-04-07 21:42:46 +0300

    Added close popup

--- src/test/java/sportdirectsearch/SportdirectSearchTest.java
+++ src/test/java/sportdirectsearch/SportdirectSearchTest.java
@@ -1,8 +1,9 @@
 package sportdirectsearch;
 
-import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -16,19 +17,18 @@ public class SportdirectSearchTest {
 
     @Test
     public void sportdirectsearch() {
-        System.setProperty("webdriver.gecko.driver", "//Users/elinajudelovica/Downloads/geckodriver");
+        System.setProperty("webdriver.gecko.driver", "/Users/elinkin/Downloads/QA course/geckodriver");
         WebDriver driver = new FirefoxDriver();
         driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("http://lv.sportsdirect.com//");
-        ((JavascriptExecutor) driver).executeScript( "window.close()" );
-        //Alert alert = driver.switchTo().alert();
-        //alert.dismiss();
-        //WebElement close = driver.findElement(By.className("close"));
-        //WebElement close = driver.findElement(By.xpath("//[button[@class='close']"));
-        //WebDriver close = driver.findElement(By.xpath("//div[contains(@class='close']"));
-        //close.click();
-        //element.submit();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement searchresult = wait.until(ExpectedConditions.elementToBeClickable(By.id("advertPopup")));
+        //WebElement close = driver.findElement(By.id("advertPopup"));
+        WebElement close = driver.findElement(By.xpath("//button[@class='close']"));
+        //searchresult.submit();
+        close.submit();
+
         //WebDriverWait wait = new WebDriverWait(driver, 10);
         //WebElement searchresult = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("JavaGuru - Software Development Courses")));
         //Assert.assertTrue(searchresult.getText().contains("JavaGuru"));
